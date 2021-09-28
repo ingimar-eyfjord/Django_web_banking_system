d875a5881fdf69f0c6dffc432986f6497d1fe532
Linux User <Tiffany@kea-dev.members.linode.com>
2021-09-28 10:34:25 +0200

    account models created

--- banking_app/models.py
+++ banking_app/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+class Account(models.Model):
+    account_id = models.IntegerField(max_length=50)
+    is_loan = models.BooleanField()
+    username = models.CharField(max_length=150)
