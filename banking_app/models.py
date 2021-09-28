9ab7969a2610ec345275528df3594e51a5ef1be5
Linux User <rachelvo@kea-dev-2.members.linode.com>
2021-09-28 10:34:43 +0200

    added Customer obj

--- banking_app/models.py
+++ banking_app/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from phonenumber_filed.modelfields import PhoneNumberField
 
-# Create your models here.
+
+class Customer(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    email = models.EmailField(max_length=254)
+    ranking = models.CharField(max_length=30)
+    phone_number = PhoneNumberField()
+
+
+
