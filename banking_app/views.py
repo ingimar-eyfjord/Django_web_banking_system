8cd63b840354563bbce2a2775b6d8814ccc3edac
ingimar-eyfjord <ingimareys93@gmail.com>
2021-09-28 10:15:23 +0000

    added signup view funtion

--- banking_app/views.py
+++ banking_app/views.py
@@ -10,3 +10,23 @@ def index(request):
 @login_required
 def user_account(request, pk):
     return render(request, 'banking_app_templates/accounts.html', context)
+
+def signup(request):
+    context = {}
+    if request.method == "POST":
+        password = request.POST['password']
+        confirm_password = request.POST['confirm_password']
+        user_name = request.POST['user']
+        email = request.POST['user']
+        if password == confirm_password:
+            if User.objects.create_user(user_name, email, password):
+                return render(request, 'mkgif/index.html', context)
+            else:
+                context = {
+                        'error' : 'Could not create user account - please try again.'
+                        }
+            else:
+                context = {
+                        'error' : 'Passwords did not match - please try again'
+                        }
+    return render(request, 'registration/signup.html', context)
