0b128a7fb5835971d9f2f5e05d10948c1c928886
ingimar-eyfjord <ingimareys93@gmail.com>
2021-09-28 11:23:25 +0000

    done some modeling

--- banking_app/models.py
+++ banking_app/models.py
@@ -3,25 +3,21 @@ from phonenumber_filed.modelfields import PhoneNumberField
 
 
 class Customer(models.Model):
-    username = models.CharField(max_length=150)
-    first_name = models.CharField(max_length=50)
-    last_name = models.CharField(max_length=50)
-    email = models.EmailField(max_length=254)
+    user = models.OneToOneField(User, primary_key=True)
     ranking = models.CharField(max_length=30)
     phone_number = PhoneNumberField()
 
-
-
-class Account(models.Model):
-    account_id = models.IntegerField(max_length=50)
-    is_loan = models.BooleanField(False)
-    username = models.CharField(max_length=150)
-
-
 class Ledger(models.Model):
-    credit = models.IntegerField(max_length=225)
-    debit = models.IntegerField(max_length=225)
+    account_id = models.ForeignKey(Account)
+    amount = models.IntegerField(max_length=225)
     transaction_date = models.DateTimeField.auto_now_add
     transaction_id = models.IntegerField(max_length=150)
 
+class Account(models.Model):
+    customer_id = models.ForeignKey(Customer)
+    is_loan = models.BooleanField(False)
+
+    @property
+    def balance(self):
+    return Ledger.object.filter(account_id=self).aggragate(Sum('amount'))
 
