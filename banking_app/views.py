b871eaddf2440e0aed35685c0bff0409dbf2b023
ingimar-eyfjord <ingimareys93@gmail.com>
2021-09-28 09:55:22 +0000

    added views

--- banking_app/views.py
+++ banking_app/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from .models import Customer, Accounts
+from django.contrib.auth.models import User
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 
-# Create your views here.
+def index(request):
+    return render(request, 'banking_app_templates/index.html', context)
+
+@login_required
+def user_account(request, pk):
+    return render(request, 'banking_app_templates/accounts.html', context)
