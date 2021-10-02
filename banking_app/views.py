293407b0dcccb66510d54f42ed31d11309170cf9
Ingimar <ingimareys93@gmail.com>
2021-10-02 16:13:18 +0200

    signup complete

--- banking_app/views.py
+++ banking_app/views.py
@@ -5,6 +5,8 @@ from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponseRedirect
 from django.urls import reverse
+from datetime import date
+from datetime import datetime
 
 def index(request):
     context = {
@@ -41,23 +43,27 @@ def CreateAUser(request):
         if request.method == "POST":
             password = request.POST['password']
             confirm_password = request.POST['confirm_password']
-            username = request.POST['Username']
-            firstName = request.POST['First_name']
-            Last_name = request.POST['Last_name']
-            email = request.POST['email']
             is_active = True
-            last_login= now
-            date_joined = now
+            last_login= datetime.now()
+            date_joined = date.today()
             is_staff = False
             if password == confirm_password:
-                if User.objects.create_user(username, email, password, is_active, last_login, date_joined, is_staff, firstName, Last_name):
-                    return render(request, 'banking_templates/index.html', context)
+                if User.objects.create_user(username=request.POST['username'], email=request.POST['email'], password=request.POST['password'], is_active=is_active, last_login=last_login, date_joined=date_joined, is_staff=is_staff, first_name=request.POST['First_name'], last_name=request.POST['Last_name']):
+                    context = {
+                        "status": 200,
+                        "message": "User has been successfully created"
+                    }
+                    return render(request, 'banking_templates/staff_home.html', context)
                 else:
                     context = {
+                    "status": 400,
                     'error' : 'Could not create user account - please try again.'
                     }
         else:
             context = {
+                "status": 400,
                 'error' : 'Passwords did not match - please try again'
                 }
-    return render(request, 'registration/staff_home.html', context)
+        return render(request, 'registration/staff_home.html', context)
+    else:
+        return render(request, 'registration/index.html', context)
