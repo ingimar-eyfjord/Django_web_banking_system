eee2a8a0985a3b6de4811efce6dd0948bed7c7a4
Tiffvoli <tiffany.tran1212@gmail.com>
2021-10-03 15:15:21 +0200

    changes to views, index, login, staff_home

--- banking_app/admin.py
+++ banking_app/admin.py
@@ -1,10 +1,6 @@
 from django.contrib import admin
-<<<<<<< HEAD
-from django.contrib.auth.models import User
-
-=======
 from .models import Customer, Account, Ledger
->>>>>>> 6ffb920ab2e9602779976eb75074e6b5b2ba7d27
+from django.contrib.auth.models import User
 
 # Register your models here.
 admin.site.register(Customer)
