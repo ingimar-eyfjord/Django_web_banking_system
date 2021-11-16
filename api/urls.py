a71ab855c0add022363739c6924a3e1f41acfeb4
ingimar-eyfjord <ingimareys93@gmail.com>
2021-11-16 09:37:03 +0100

    api needs to use PK, so testing with GET does not work because it assumes you are fetching, so resting the create/account needs to be tested with POST

--- api/urls.py
+++ api/urls.py
@@ -5,6 +5,6 @@ from bank import views
 app_name = "api"
 
 urlpatterns = [
-    path('transaction/create/account/<int:pk>/ammount/<int:ammount>', views.Api_create_transaction.as_view()),
+    path('transaction/create/account/<int:account>/ammount/<int:ammount>', views.Api_create_transaction.as_view()),
     path('transaction/receive/account/<int:pk>/ammount/<int>', views.Api_create_transaction.as_view()),
 ]
